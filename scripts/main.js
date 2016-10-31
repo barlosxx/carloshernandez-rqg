2b9f3ce0657f4886324c5bd74a91e457535903d4
Carlos Hernandez <cmhernandez00@gmail.com>
2016-10-31 19:26:01 +0000

    Created Array in JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var header = document.getElementById('header');
 
-header.style = "background:#777; border:1px solid #yellow;";
+header.style = "background:#777; border:1px solid Red;";
 
 var title = document.createElement("h1");
 title.textContent = "Random Tip Generator";
@@ -14,3 +14,14 @@ header.appendChild(description);
 
 var quoteArea = document.createElement("div");
 quoteArea.setAttribute("id","quoteArea");
+
+var dataTips = []
+
+dataTips = [
+    {tip: "Everyone dies eventually."},
+    {tip: "Live for you."},
+    {tip: "Make moves."},
+    {tip: "Find out whats important to you and work for it."},
+    {tip: "Who are you".},
+];
+
