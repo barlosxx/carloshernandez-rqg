66f568c2f1ee6e92ba59d30c08f3e9a3873f179c
Carlos Hernandez <cmhernandez00@gmail.com>
2016-10-31 19:53:28 +0000

    added scripts/main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 var header = document.getElementById('header');
 
-header.style = "background:#777; border:1px solid Red;";
+header.style = "background:#777; border:1px solid black; text-align: center; margin: 0px auto;";
 
 var title = document.createElement("h1");
 title.textContent = "Random Tip Generator";
@@ -8,7 +8,7 @@ title.textContent = "Random Tip Generator";
 header.appendChild(title);
 
 var description = document.createElement("p");
-description.innerHTML = "This is a description";
+description.innerHTML = "This is a description.";
 
 header.appendChild(description);
 
@@ -22,6 +22,8 @@ dataTips = [
     {tip: "Live for you."},
     {tip: "Make moves."},
     {tip: "Find out whats important to you and work for it."},
-    {tip: "Who are you".},
+    {tip: "Who are you"},
 ];
+
+console.log(dataTips.length);
 
